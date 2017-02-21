cf4e89ddacae87869371fc1c05e18e3d61bc2d73
Andrew Hill <ahill7@ix-xenial.cs.uoregon.edu>
2017-02-20 16:46:21 -0800

    linked flask to sql server

--- main.py
+++ main.py
@@ -2,12 +2,35 @@ import flask
 from flask import Flask
 from flask import render_template
 from flask import request
+from flaskext.mysql import MySQL
 app = Flask(__name__)
+
+
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'guest'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'guest'
+app.config['MYSQL_DATABASE_DB'] = 'kaby'
+app.config['MYSQL_DATABASE_HOST'] = 'ix.cs.uoregon.edu'
+app.config['MYSQL_DATABASE_PORT'] = 3225
+mysql.init_app(app)
+
+conn = mysql.connect()
 
 @app.route('/')
 @app.route('/login')
 def login():
+    cur = conn.cursor()
+    cur.execute("SELECT * from user")
+
+    rows = cur.fetchall()
+    conn.close()
+
+    for row in rows:
+		print row
     return render_template('login.html') 
+	
 
 
 @app.route('/home')
@@ -25,3 +48,5 @@ def form_action():
 	date = request.form.get('date')
 	print(date);
 	return flask.redirect(flask.url_for("home"))
+
+
