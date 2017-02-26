77b94e94855271545388c3d8efe253aabd1214d5
ahill7 <ahill7@uoregon.edu>
2017-02-26 03:15:01 -0800

    Create and add new meeting to data base

--- main.py
+++ main.py
@@ -25,7 +25,11 @@ conn.close()
 
 """
 
-group_leaders = [1,2,3]
+group_leaders = []
+dates = []
+num_dates=0;
+cur_meeting_id=0;
+length_min=0
 
 def get_group_leaders():
 	group_leaders 
@@ -41,9 +45,6 @@ def get_group_leaders():
 @app.route('/')
 @app.route('/login')
 def login():
-	
-    #for row in rows:
-		#print row
     return render_template('login.html') 
 	
 
@@ -57,18 +58,47 @@ def newMeeting():
 	get_group_leaders()
 	for o in group_leaders:
 		print o
-	return render_template('newMeeting.html') 
+	return render_template('newMeeting.html',  leaders=group_leaders)
 
 
 @app.route('/newContact')
 def newContact():
     return render_template('newContact.html') 
 
+
+@app.route('/timePage')
+def timePage():
+    return render_template('time_picker.html') 
+
+
+
+
 @app.route('/form_action', methods=['POST'])
 def form_action():
-	date = request.form.get('date')
-	print(date);
-	return flask.redirect(flask.url_for("home"))
+    global dates
+    global num_dates
+    global cur_meeting_id;
+    global length_min
+    date = request.form.get('date')
+    dates = date.split(",");
+    num_dates = len(dates)
+    print("num dates entered: {}".format(num_dates))
+    for d in dates:
+        print "date:{}".format(d)
+    name = request.form.get('mName')
+    desc = request.form.get('desc')
+    loc= request.form.get('loc')
+    length_min= request.form.get('meeting_len')
+    print("length : {}".format(length_min));
+    conn =  mysql.connect()
+    cur = conn.cursor()
+    query_string = "insert into meetings (meeting_id, location, description) values ({}, '{}', '{}')".format("Null",loc, desc)
+    cur.execute(query_string)
+    cur_meeting_id = cur.lastrowid
+    print "row id : {}".format(cur_meeting_id);
+    conn.commit()
+    conn.close()
+    return render_template('time_picker.html', dates=dates) 
 
 
 @app.route('/login_action', methods=['POST'])
@@ -91,11 +121,30 @@ def login_action():
 	return flask.redirect(flask.url_for("login"))
 
 
+@app.route('/newmeeting_action', methods=['POST'])
+def newmeeting_action():
+    cntr=0;
+    global num_dates;
+    global length_min
+    global cur_meeting_id
+    global dates
+    conn =  mysql.connect()
+    cur = conn.cursor()
+    for i in range(num_dates):
+        print("global dates[i]={}".format(dates[i]))
+        name = "t{}".format(i+1);
+        day, month, year = dates[i].split("/");
+        sql_date='{}-{}-{}'.format(year, month,day);
+	time = request.form.get(name)
+        sql_time='{}:00:00'.format(time)
+        print sql_time 
+        query_string = "insert into meetings_times_dates (issue_ID, meeting_id, start_time, date, length) values ({}, {}, '{}', '{}', {});".format("NULL", cur_meeting_id, sql_time, sql_date, int(length_min));
+        cur.execute(query_string)
+        print "row id : {}".format(cur_meeting_id);
+        conn.commit()
+    conn.close()
+    return flask.redirect(flask.url_for("home"))
 
-@app.route('/newMeeting')
-def a():
-    print("HELLO")
-    return render_template('newMeeting.html') 
 
 
 #################
