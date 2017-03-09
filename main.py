c05876255240898f08a6073969dc52f120a64401
Andrew Hill <ahill7@ix-xenial.cs.uoregon.edu>
2017-03-08 23:18:56 -0800

    after all users have reponded it now emails me

--- main.py
+++ main.py
@@ -77,7 +77,6 @@ def before_action():
     #elif request.path == '/login_action':
     #    page = False
     #elif page = re.match("([0-9A-z]+)([@]+)([0-9A-z]+)([.]+)([0-9A-z]+)", email) != None
-
     #elif page = re.match("(\Ahttp://ix.cs.uoregon.edu:5951/respond/)", email) != None
     session.permanent = True
     app.permanent_session_lifetime = timedelta(minutes=10)
@@ -134,7 +133,7 @@ def view_meeting(meeting_id):
 
     #print(meeting_id)  
     #print("leader {}".format(get_leaders_for_meetingID(meeting_id)))
-     # Dummy info for development
+    #Dummy info for development
     meeting_info = get_meeting_info(meeting_id)
     print("meeting info {}".format(meeting_info))
     m_id=uuid_to_meeting_id(meeting_id); #meeting_id here is uuid_url, shoudl change.
@@ -267,20 +266,19 @@ def newmeeting_action():
     cur = conn.cursor()
     for i in range(num_dates):
         print("global dates[i]={}".format(dates[i]))
-        name = "t{}".format(i+1);
-        day, month, year = dates[i].split("/");
-        sql_date='{}-{}-{}'.format(year, month,day);
+        name = "t{}".format(i+1)
+        day, month, year = dates[i].split("/")
+        sql_date='{}-{}-{}'.format(year, month,day)
         time = request.form.get(name)
         sql_time='{}:00:00'.format(time)
         print (sql_time) 
-        #query_string = "insert into meetings_times_dates (issue_ID, meeting_id, start_time, date, length) values ({}, {}, '{}', '{}', {});".format("NULL", cur_meeting_id, sql_time, sql_date, int(length_min));
-        time_ranges = time.split(',');
+        time_ranges = time.split(',')
         for t in time_ranges:
-            start_time, end_time = t.split('-');
+            start_time, end_time = t.split('-')
             print("start {} end {}".format(start_time, end_time)) 
-            sql_start_time = '{}:00:00'.format(start_time) 
-            sql_end_time= '{}:00:00'.format(end_time) 
-            query_string = "INSERT INTO `kaby`.`dates_times` (`dt_id`, `meeting_id`, `start_time`, `end_time`, `meeting_date`) VALUES ({}, {}, '{}', '{}', '{}');".format("NULL", cur_meeting_id, sql_start_time.strip('0'), sql_end_time , sql_date);
+            sql_start_time = '{}:00:00'.format(start_time.strip("0"))
+            sql_end_time= '{}:00:00'.format(end_time.strip("0")) 
+            query_string = "INSERT INTO `kaby`.`dates_times` (`dt_id`, `meeting_id`, `start_time`, `end_time`, `meeting_date`) VALUES ({}, {}, '{}', '{}', '{}');".format("NULL", cur_meeting_id, sql_start_time.strip('0'), sql_end_time , sql_date)
             cur.execute(query_string)
             print ("row id : {}".format(cur_meeting_id))
             conn.commit()
@@ -297,14 +295,22 @@ def respond_meeting():
     user_name = request.form.get('user_name')
     available_times = request.form.getlist('available_times')
     available_times = [x.encode('ascii','ignore') for x in available_times] #convert from unicode to str
-
     print("Username: " + user_name)
     print("Available times: ", available_times)
-    user_id= group_leader_email_to_id(user_name);
+    user_id= group_leader_email_to_id(user_name)
     print("\n\n user name {} to user id {}".format(user_name, user_id))
     print("resp id {}".format(response_meeting))
-
     insert_user_response(user_id ,response_meeting, available_times)
+    m_id=uuid_to_meeting_id(response_meeting); #meeting_id here is uuid_url, shoudl change.
+    num_not_resp = get_num_non_responded(m_id)
+    email_list=["ahill7@uoregon.edu"]
+    link="http://ix.cs.uoregon.edu:5951/view_meeting/" + response_meeting
+    print("\n\n number not responded = {} \n\n").format(num_not_resp);
+    if(num_not_resp == 0):
+        print("email prfessor")
+        send_message("All groups have responded", link, email_list);
+    else:
+        print("dont email yet")
     return flask.redirect(flask.url_for("thanks"))
 
 
@@ -520,13 +526,31 @@ def get_not_responded(meeting_uuid):
     query_string = "Select group_name from respond_meeting join group_leader on(group_id=group_leader_id) where issue_ID={} and group_id not in (select group_leader_id from response where dt_id={});".format(meeting_uuid, meeting_uuid);
     conn =  mysql.connect()
     cur = conn.cursor()
+    num_not_respond=0;
     cur.execute(query_string)
     non_respond = cur.fetchall()
     for i in non_respond:
+        num_not_respond= num_not_respond + 1
         print("not resp {}".format(i))
         not_responded.append(i[0])
     conn.close()
+    print("num not resp  ={}".format(num_not_respond));
     return not_responded 
+
+
+def get_num_non_responded(meeting_uuid):
+    not_responded=[];
+    query_string = "Select count(*) from respond_meeting join group_leader on(group_id=group_leader_id) where issue_ID={} and group_id not in (select group_leader_id from response where dt_id={});".format(meeting_uuid, meeting_uuid);
+    conn =  mysql.connect()
+    cur = conn.cursor()
+    num_not_respond=0;
+    cur.execute(query_string)
+    non_respond = cur.fetchone()
+    conn.close()
+    print("num not resp={}".format(non_respond[0]));
+    return non_respond[0] 
+
+
 
 def get_meeting_info(meeting_uuid):
     query_string="select location, meeting_name, description from meetings where uuid='{}'; ".format(meeting_uuid)
@@ -553,7 +577,6 @@ def get_responded(meeting_id):
         else:
             resp[i[3]] = [(i[0], i[1], i[2])];
     conn.close()
-
     return resp 
 
 
